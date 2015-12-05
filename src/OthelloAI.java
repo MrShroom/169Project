0bfceb9de4e225efb055a2e144f9b9d55c9ea487
Shaun McThomas <shaunrm23@gmail.com>
2015-12-05 12:45:34 -0800

    Adding OthelloAI
    This is to fix up stuff. Not  really adding much

---
+++ src/OthelloAI.java
@@ -0,0 +1,19 @@
+import java.awt.Point;
+import java.util.List;
+
+public class OthelloAI implements AI {
+	
+	List<Double> heuristicPramameters;
+	
+	public OthelloAI(List<Double> heuristicPramameters)
+	{
+		this.heuristicPramameters = heuristicPramameters;
+	}
+
+	@Override
+	public Point makeMove(Reversi game) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
