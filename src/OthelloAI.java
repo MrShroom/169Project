8a12b9ad61d9930619b9488fbc0354010dab98e0
bakester14 <bakester14@gmail.com>
2015-12-05 13:47:04 -0800

    Othello AI spelling and corrections
    Changed heuristicParameters into two separate variables, Double
    heuristicFlip (for # flipped) and Double[][] heuristicMap (for [0-8],
    the weights for board positions). Added a testing void main.

--- src/OthelloAI.java
+++ src/OthelloAI.java
@@ -3,34 +3,51 @@ import java.util.List;
 
 public class OthelloAI implements AI {
 	
-	List<Double> heuristicParameters;
+	Double heuristicFlip;
 	Double[][] heuristicMap;
 	
-	public OthelloAI(List<Double> hp)
+	// A quick test of the constructor
+	public static void main(String [] args)
+	{
+		// Print a test heuristic map
+		Double[] argument = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+		OthelloAI x = new OthelloAI(argument);
+		
+		for (int i = 0; i < 8; i ++){
+			for (int j = 0; j < 8; j++){
+				System.out.print(x.heuristicMap[i][j]);
+			}
+			System.out.println();
+		}
+		
+		// Other testing here
+	}
+	
+	public OthelloAI(Double[] hp)
 	{
 		// Initialize private variables
-		this.heuristicParameters = hp;
+		this.heuristicFlip = hp[9];
 		this.heuristicMap = new Double[8][8];
 		
 		// Initialize upper left quadrant of heuristicMap with values
-		this.heuristicMap[0][0] = hp.get(0);
-		this.heuristicMap[1][0] = hp.get(1);
-		this.heuristicMap[2][0] = hp.get(2);
-		this.heuristicMap[3][0] = hp.get(3);
+		this.heuristicMap[0][0] = hp[0];
+		this.heuristicMap[1][0] = hp[1];
+		this.heuristicMap[2][0] = hp[2];
+		this.heuristicMap[3][0] = hp[3];
 		
-		this.heuristicMap[0][1] = hp.get(1);
-		this.heuristicMap[1][1] = hp.get(4);
-		this.heuristicMap[2][1] = hp.get(5);
-		this.heuristicMap[3][1] = hp.get(6);
+		this.heuristicMap[0][1] = hp[1];
+		this.heuristicMap[1][1] = hp[4];
+		this.heuristicMap[2][1] = hp[5];
+		this.heuristicMap[3][1] = hp[6];
 		
-		this.heuristicMap[0][2] = hp.get(2);
-		this.heuristicMap[1][2] = hp.get(5);
-		this.heuristicMap[2][2] = hp.get(7);
-		this.heuristicMap[3][2] = hp.get(8);
+		this.heuristicMap[0][2] = hp[2];
+		this.heuristicMap[1][2] = hp[5];
+		this.heuristicMap[2][2] = hp[7];
+		this.heuristicMap[3][2] = hp[8];
 		
-		this.heuristicMap[0][3] = hp.get(3);
-		this.heuristicMap[1][3] = hp.get(6);
-		this.heuristicMap[2][3] = hp.get(8);
+		this.heuristicMap[0][3] = hp[3];
+		this.heuristicMap[1][3] = hp[6];
+		this.heuristicMap[2][3] = hp[8];
 		this.heuristicMap[3][3] = -1.0;
 		
 		// Mirror values from upper left quadrant to every other quadrant
